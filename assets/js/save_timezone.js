b7c4ff1b9119800682721c7af4754482ce2250ae
Dharmesh Patel <dspatel44@gmail.com>
2024-03-05 17:10:25 +0530

    Fix eslint errors in save_timezone.js

--- assets/js/save_timezone.js
+++ assets/js/save_timezone.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 if (navigator.cookieEnabled && Intl !== undefined) {
 	document.cookie =
 		'wgc_timezone=' +
@@ -8,8 +9,8 @@ if (navigator.cookieEnabled && Intl !== undefined) {
 jQuery(document).on(
 	'change',
 	'input[type=radio][name=elavon-converge-gateway_stored_card]:checked',
-	function (e) {
-		if (this.value == 'elavon-converge-gateway_new_card') {
+	function () {
+		if (this.value === 'elavon-converge-gateway_new_card') {
 			jQuery('.save-for-later-use').show();
 			jQuery('#wc-elavon-converge-gateway-cc-form').show();
 		} else {
@@ -22,7 +23,7 @@ jQuery(document).on(
 if (
 	jQuery(
 		'input[type=radio][name=elavon-converge-gateway_stored_card]:checked'
-	).val() == 'elavon-converge-gateway_new_card'
+	).val() === 'elavon-converge-gateway_new_card'
 ) {
 	jQuery('.save-for-later-use').show();
 	jQuery('#wc-elavon-converge-gateway-cc-form').show();
