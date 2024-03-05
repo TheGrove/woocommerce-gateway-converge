660f5d901a892ccd2351117a3411a94311b1d39e
faisal-alvi <faisalalvi33@gmail.com>
2024-03-05 21:37:48 +0530

    FILE: /Users/faisalalvi/LocalSites/wpne/app/public/wp-content/plugins/woocommerce-gateway-converge/includes/admin/meta-boxes/views/html-converge-subscription-details.php ------------------------------------------------------------------------------------------------------------------------------------------------------------------------- FOUND 7 ERRORS AFFECTING 7 LINES -------------------------------------------------------------------------------------------------------------------------------------------------------------------------   5 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$converge_subscription'.   9 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$converge_subscription'.  13 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found 'wgc_format_datetime'.  17 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found 'wgc_format_datetime'.  21 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found 'wgc_format_datetime'.  25 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$converge_subscription'.  29 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$converge_subscription'. -------------------------------------------------------------------------------------------------------------------------------------------------------------------------

--- includes/admin/meta-boxes/views/html-converge-subscription-details.php
+++ includes/admin/meta-boxes/views/html-converge-subscription-details.php
@@ -2,31 +2,31 @@
 	<table>
 		<tr>
 			<td><strong><?php _e( 'Subscription ID', 'elavon-converge-gateway' ); ?>:</strong></td>
-			<td><?php echo $converge_subscription->id; ?></td>
+			<td><?php echo esc_html( $converge_subscription->id ); ?></td>
 		</tr>
 		<tr>
 			<td><strong><?php _e( 'Subscription State', 'elavon-converge-gateway' ); ?>:</strong></td>
-			<td><?php echo $converge_subscription->subscriptionState; ?></td>
+			<td><?php echo esc_html( $converge_subscription->subscriptionState ); ?></td>
 		</tr>
 		<tr>
 			<td><strong><?php _e( 'Start Date', 'elavon-converge-gateway' ); ?>:</strong></td>
-			<td><?php echo wgc_format_datetime( $converge_subscription->firstBillAt ); ?></td>
+			<td><?php echo esc_html( wgc_format_datetime( $converge_subscription )->firstBillAt ); ?></td>
 		</tr>
 		<tr>
 			<td><strong><?php _e( 'Next Payment Date', 'elavon-converge-gateway' ); ?>:</strong></td>
-			<td><?php echo $converge_subscription->nextBillAt ? wgc_format_datetime( $converge_subscription->nextBillAt ) : '-'; ?></td>
+			<td><?php echo $converge_subscription->nextBillAt ? esc_html( wgc_format_datetime( $converge_subscription->nextBillAt ) ) : '-'; ?></td>
 		</tr>
 		<tr>
 			<td><strong><?php _e( 'End Date', 'elavon-converge-gateway' ); ?>:</strong></td>
-			<td><?php echo $converge_subscription->finalBillAt ? wgc_format_datetime( $converge_subscription->finalBillAt ) : '-'; ?></td>
+			<td><?php echo $converge_subscription->finalBillAt ? esc_html( wgc_format_datetime( $converge_subscription->finalBillAt ) ) : '-'; ?></td>
 		</tr>
 		<tr>
 			<td><strong><?php _e( 'Timezone', 'elavon-converge-gateway' ); ?>:</strong></td>
-			<td><?php echo $converge_subscription->timeZoneId; ?></td>
+			<td><?php echo esc_html( $converge_subscription->timeZoneId ); ?></td>
 		</tr>
 		<tr>
 			<td><strong><?php _e( 'Failure Count', 'elavon-converge-gateway' ); ?>:</strong></td>
-			<td><?php echo $converge_subscription->failureCount; ?></td>
+			<td><?php echo esc_html( $converge_subscription->failureCount ); ?></td>
 		</tr>
 	</table>
 </div>
