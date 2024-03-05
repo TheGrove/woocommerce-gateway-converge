af360da3d90c9f6c8f5a59ac30691335293f1fc5
faisal-alvi <faisalalvi33@gmail.com>
2024-03-05 18:22:02 +0530

    Bump "tested up to" WordPress 6.4

--- woocommerce-gateway-converge.php
+++ woocommerce-gateway-converge.php
@@ -21,6 +21,8 @@
  * Author URI:        http://www.elavon.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Tested up to:      6.4
+ * Requires at least: 6.3
  * Text Domain:       elavon-converge-gateway
  * Domain Path:       /languages
  */
