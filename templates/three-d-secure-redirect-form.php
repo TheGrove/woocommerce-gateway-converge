dc331f6f97e95f6d211a826b57eaddbc3cc9fca2
faisal-alvi <faisalalvi33@gmail.com>
2024-03-06 22:02:59 +0530

    FILE: /Users/faisalalvi/LocalSites/wpne/app/public/wp-content/plugins/woocommerce-gateway-converge/templates/three-d-secure-redirect-form.php --------------------------------------------------------------------------------------------------------------------------------------------------------------------------- FOUND 5 ERRORS AFFECTING 4 LINES ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------  14 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$form_id'.  14 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$accessControlServerUrl'.  15 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$payerAuthenticationRequest'.  16 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$termUrl'.  20 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$form_id'. ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------

--- templates/three-d-secure-redirect-form.php
+++ templates/three-d-secure-redirect-form.php
@@ -11,11 +11,11 @@ if (
 $form_id = 'converge-three-d-secure-redirect';
 ?>
 
-<form id="<?php echo $form_id; ?>" action="<?php echo $accessControlServerUrl; ?>" method="post">
-	<input name="PaReq" type="hidden" value="<?php echo $payerAuthenticationRequest; ?>"/>
-	<input name="TermUrl" type="hidden" value="<?php echo $termUrl; ?>"/>
+<form id="<?php echo esc_attr( $form_id ); ?>" action="<?php echo esc_url( $accessControlServerUrl ); ?>" method="post">
+	<input name="PaReq" type="hidden" value="<?php echo esc_attr( $payerAuthenticationRequest ); ?>"/>
+	<input name="TermUrl" type="hidden" value="<?php echo esc_url( $termUrl ); ?>"/>
 </form>
 
 <script type="text/javascript">
-	document.getElementById('<?php echo $form_id; ?>').submit();
+	document.getElementById('<?php echo esc_html( $form_id ); ?>').submit();
 </script>
