6b16c6d4563deb2e1888dc889ba57de98aa7ec7e
faisal-alvi <faisalalvi33@gmail.com>
2024-03-06 21:59:34 +0530

    FILE: /Users/faisalalvi/LocalSites/wpne/app/public/wp-content/plugins/woocommerce-gateway-converge/templates/checkout/payment-method.php -------------------------------------------------------------------------------------------------------------------------------------------------------------------------- FOUND 18 ERRORS AFFECTING 14 LINES --------------------------------------------------------------------------------------------------------------------------------------------------------------------------  29 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$gateway'.  29 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$token'.  30 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$gateway'.  31 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$token'.  52 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$gateway'.  52 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$token'.  52 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$token'.  56 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$gateway'.  57 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$gateway'.  58 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$gateway'.  66 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$gateway'.  77 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found 'WGC_KEY_SAVE_FOR_LATER_USE'.  78 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found 'WGC_KEY_SAVE_FOR_LATER_USE'.  79 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found 'WGC_KEY_SAVE_FOR_LATER_USE'.  80 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$gateway'.  83 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found 'WGC_KEY_SAVE_FOR_LATER_USE'.  83 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found 'WGC_KEY_SAVE_FOR_LATER_USE'.  93 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found 'stripslashes'. --------------------------------------------------------------------------------------------------------------------------------------------------------------------------

--- templates/checkout/payment-method.php
+++ templates/checkout/payment-method.php
@@ -26,9 +26,9 @@
 					endif;
 					?>
 
-					<div><input type="radio" class="input-radio" id="<?php echo $gateway->id . $token->get_id(); ?>"
-								name="<?php echo $gateway->stored_card_key; ?>"
-								value="<?php echo $token->get_id(); ?>"
+					<div><input type="radio" class="input-radio" id="<?php echo esc_attr( $gateway->id . $token->get_id() ); ?>"
+								name="<?php echo esc_attr( $gateway->stored_card_key ); ?>"
+								value="<?php echo esc_attr( $token->get_id() ); ?>"
 							<?php
 							if ( $token->is_expired() ) {
 								echo 'disabled';
@@ -49,13 +49,13 @@
 							?>
 							>
 						</input>
-						<label for="<?php echo $gateway->id . $token->get_id(); ?>"><?php echo $token->get_display_name(); ?></label>
+						<label for="<?php echo esc_attr( $gateway->id . $token->get_id() ); ?>"><?php echo esc_html( $token->get_display_name() ); ?></label>
 					</div>
 				<?php endforeach; ?>
 				<div>
-					<input type="radio" class="input-radio use-new-card" id="<?php echo $gateway->new_card_key; ?>"
-							name="<?php echo $gateway->stored_card_key; ?>"
-							value="<?php echo $gateway->new_card_value; ?>"
+					<input type="radio" class="input-radio use-new-card" id="<?php echo esc_attr( $gateway->new_card_key ); ?>"
+							name="<?php echo esc_attr( $gateway->stored_card_key ); ?>"
+							value="<?php echo esc_attr( $gateway->new_card_value ); ?>"
 						<?php
 						if ( $default_is_expired ) {
 							echo 'checked';
@@ -63,7 +63,7 @@
 						?>
 						>
 					</input>
-					<label for="<?php echo $gateway->new_card_key; ?>"><?php _e( 'Use new card', 'elavon-converge-gateway' ); ?></label>
+					<label for="<?php echo esc_attr( $gateway->new_card_key ); ?>"><?php _e( 'Use new card', 'elavon-converge-gateway' ); ?></label>
 				</div>
 			<?php endif; ?>
 
@@ -74,13 +74,13 @@
 					?>
 					style="display: none"<?php endif; ?>>
 					<?php if ( ( ! wgc_has_subscription_elements_in_cart() && ! wgc_order_from_merchant_view_has_subscription_elements() ) && ! $gateway->is_subscription_change_method_page() ) : ?>
-						<input type="checkbox" id="<?php echo WGC_KEY_SAVE_FOR_LATER_USE; ?>"
-								name="<?php echo WGC_KEY_SAVE_FOR_LATER_USE; ?>" value="1"/>
-						<label for="<?php echo WGC_KEY_SAVE_FOR_LATER_USE; ?>"><?php _e( 'Save for later use', 'elavon-converge-gateway' ); ?></label>
-						<p class="save-for-later-use-message"><?php echo $gateway->get_option( WGC_KEY_SAVE_FOR_LATER_USE_MESSAGE ); ?></p>
+						<input type="checkbox" id="<?php echo esc_html( WGC_KEY_SAVE_FOR_LATER_USE ); ?>"
+								name="<?php echo esc_html( WGC_KEY_SAVE_FOR_LATER_USE ); ?>" value="1"/>
+						<label for="<?php echo esc_html( WGC_KEY_SAVE_FOR_LATER_USE ); ?>"><?php esc_html_e( 'Save for later use', 'elavon-converge-gateway' ); ?></label>
+						<p class="save-for-later-use-message"><?php echo esc_html( $gateway->get_option( WGC_KEY_SAVE_FOR_LATER_USE_MESSAGE ) ); ?></p>
 					<?php else : ?>
 						<input
-						type="hidden" id="<?php echo WGC_KEY_SAVE_FOR_LATER_USE; ?>" name="<?php echo WGC_KEY_SAVE_FOR_LATER_USE; ?>" value="1">
+						type="hidden" id="<?php echo esc_attr( WGC_KEY_SAVE_FOR_LATER_USE ); ?>" name="<?php echo esc_attr( WGC_KEY_SAVE_FOR_LATER_USE ); ?>" value="1">
 						<p class="save-for-later-use-message">
 							<?php _e( 'Your info and card details will be saved. Subscriptions must be tied to your profile in order to process recurring payments.', 'elavon-converge-gateway' ); ?>
 						</p>
@@ -90,7 +90,7 @@
 		</div>
 	<?php endif; ?>
 	<?php if ( ( wgc_has_subscription_elements_in_cart() && ! $gateway->is_subscription_change_method_page() ) || wgc_order_from_merchant_view_has_subscription_elements() ) : ?>
-		<p><?php echo stripslashes( $gateway->get_option( WGC_KEY_SUBSCRIPTIONS_DISCLOSURE_MESSAGE ) ); ?></p>
+		<p><?php echo esc_html( stripslashes( $gateway->get_option( WGC_KEY_SUBSCRIPTIONS_DISCLOSURE_MESSAGE ) ) ); ?></p>
 	<?php endif; ?>
 </div>
 <?php if ( $gateway->is_subscription_change_method_page() ) : ?>
