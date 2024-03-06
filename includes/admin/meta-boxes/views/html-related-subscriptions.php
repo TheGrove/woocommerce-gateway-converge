a1f4fb34ab444699d742cf427f2e9751f811d39a
faisal-alvi <faisalalvi33@gmail.com>
2024-03-06 21:38:07 +0530

    FILE: /Users/faisalalvi/LocalSites/wpne/app/public/wp-content/plugins/woocommerce-gateway-converge/includes/admin/meta-boxes/views/html-related-subscriptions.php ------------------------------------------------------------------------------------------------------------------------------------------------------------------ FOUND 4 ERRORS AFFECTING 3 LINES ------------------------------------------------------------------------------------------------------------------------------------------------------------------  14 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found 'get_edit_post_link'.  14 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$subscription'.  16 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found '$subscription'.  17 | ERROR | All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found 'wc_price'. ------------------------------------------------------------------------------------------------------------------------------------------------------------------

--- includes/admin/meta-boxes/views/html-related-subscriptions.php
+++ includes/admin/meta-boxes/views/html-related-subscriptions.php
@@ -11,10 +11,10 @@
 		<?php foreach ( (array) $subscriptions as $subscription ) : ?>
 			<tr>
 				<td>
-					<a href="<?php echo get_edit_post_link( $subscription->get_id() ); ?>">#<?php echo $subscription->get_order_number(); ?></a>
+					<a href="<?php echo esc_url( get_edit_post_link( $subscription->get_id() ) ); ?>">#<?php echo esc_html( $subscription->get_order_number() ); ?></a>
 				</td>
-				<td><?php echo $subscription->get_status(); ?></td>
-				<td><?php echo wc_price( $subscription->get_total() ); ?></td>
+				<td><?php echo esc_html( $subscription->get_status() ); ?></td>
+				<td><?php echo wp_kses_post( wc_price( $subscription->get_total() ) ); ?></td>
 			</tr>
 		<?php endforeach; ?>
 		</tbody>
